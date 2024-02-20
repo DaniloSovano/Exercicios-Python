0992fd3dbc1e4a7f54edfe82ac8af843db6d70cf
DaniloSovano <160192985+DaniloSovano@users.noreply.github.com>
2024-02-20 11:42:20 -0300

    Update Exercicio python 1.py

--- Exercicio python 1.py
+++ Exercicio python 1.py
@@ -10,10 +10,3 @@ print( f'a subtração de {Num1} - {Num2} é: {sub}')
 print(f'a multiplicação de {Num1} * {Num2} é: {multi}')
 print( f'a divisão de {Num1} por {Num2} é: {div}')
 print( f'O resto da divisão de {Num1} por {Num2} é: {rest}')
-echo "# Exercicios-Python" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/DaniloSovano/Exercicios-Python.git
-git push -u origin main
